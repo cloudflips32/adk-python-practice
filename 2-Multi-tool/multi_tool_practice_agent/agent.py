bc64890e8e04ebf5bf86fa7fbe1459afd979bfc6
Adam Standish <adamjstandishcoder@icloud.com>
2026-04-13 23:09:06 -0400

    multi-tool instruction edit

--- 2-Multi-tool/multi_tool_practice_agent/agent.py
+++ 2-Multi-tool/multi_tool_practice_agent/agent.py
@@ -17,6 +17,6 @@ root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful terminal command learning assistant for user questions.',
-    instruction='Help the user better learn how to use Windows Powershell and the Linux Bash terminal by digesting user queries and providing the correct command for their needs. Be sure to verify the correct terminal, as well as the correct command for the given task.',
+    instruction='Help the user better learn how to use Windows Powershell, the Linux Bash terminal, and the Windows Command Prompt by digesting user queries and providing the correct command for their needs. Be sure to verify the correct terminal, as well as the correct command for the given task.',
     tools=[get_powershell_commands, get_bash_commands, get_cmd_commands],
 )
